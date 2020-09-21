b4c47437c82c7ae5213a58503f1b31740dac55da
CaitlinLandrus <50267367+CaitlinLandrus@users.noreply.github.com>
2020-09-21 16:57:15 -0500

    Added server and procfile

---
+++ server.js
@@ -0,0 +1,42 @@
+//Import Modules
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+
+//Global Variables
+const app = express()
+const PORT = process.env.PORT || 5000
+
+
+//Import Files
+const routes = require('./routes')
+
+//Connect DB
+//const db = require('./backend/DB')
+//db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+
+//Configuration
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/', routes);
+
+
+//if(process.env.NODE_ENV ==='production'){
+
+    app.use(express.static(path.join(__dirname, 'build')))
+    app.set('build', path.join(__dirname, 'index.html'))
+    app .set('view engine', 'ejs')
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    })
+
+//}
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+
+module.exports = app; //Added for testing
