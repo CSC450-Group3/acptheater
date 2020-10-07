b955b3b4d4381cd6aa602280485dc27a123277e3
PGarryCS <65678578+PGarryCS@users.noreply.github.com>
2020-10-07 17:16:57 -0500

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,113 @@
 import React from 'react';
+import beer from './beer.jpeg';
+import lights from './lights.jpeg';
+import reel from './reel.jpeg';
+import seats from './seats.jpeg';
+import popcorn from './popcorn.jpeg';
+import nachos from './nachos.jpeg';
+import candy from './candy.jpeg';
 
+console.log(beer);
+console.log(lights);
+console.log(reel);
+console.log(seats);
+console.log(popcorn);
+console.log(nachos);
+console.log(candy);
 
 function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Home Page stuff...
-        </p>
-      </header>
-    </div>
+    <body>
+      <div className="welcome">
+        <h1>ACP Theatres Welcomes you!</h1>
+        <p className="welcomeIntro">
+          Brought to you by Alex Yahn, Caitlin Landrus, and
+          Patrick Garry. Happy to bring you live streaming
+          from the comforts of you own home as well as
+          in-person premier seating!
+          </p>
+      </div>
+
+      <div className="currentMovies">
+        <h1>Current Movies</h1>
+        <div class="row">
+
+          <div class="column">
+            <p>Movie Photo</p>
+            <p>Movie Title</p>
+            <p>Run Time</p>
+            <p>Release Date</p>
+          </div>
+
+          <div class="column">
+            <p>Movie Photo</p>
+            <p>Movie Title</p>
+            <p>Run Time</p>
+            <p>Release Date</p>
+          </div>
+
+          <div class="column">
+            <p>Movie Photo</p>
+            <p>Movie Title</p>
+            <p>Run Time</p>
+            <p>Release Date</p>
+          </div>
+
+          <div class="column">
+            <p>Movie Photo</p>
+            <p>Movie Title</p>
+            <p>Run Time</p>
+            <p>Release Date</p>
+          </div>
+        </div>
+
+      </div>
+
+      <div className="beverages">
+        <h1>
+          Beverages
+          </h1>
+          <p>
+            <img src={beer} alt="beer" width="75%"/>
+          </p>
+      </div>
+
+      <div className="food">
+        <h1>
+          Food
+          </h1>
+          <div class="row">
+
+          <div class="column">
+            <p><img src={popcorn} alt="popcorn" margin="auto" width="75%"/></p>
+            <p>Popcorn</p>
+            <p>Description / Sizes</p>
+            <p>Price(s)</p>
+          </div>
+
+          <div class="column">
+          <p><img src={nachos} alt="nachos" margin="auto" width="75%"/></p>
+            <p>Nachos</p>
+            <p>Description</p>
+            <p>Price</p>
+          </div>
+
+          <div class="column">
+          <p><img src={candy} alt="candy" margin="auto" alight width="75%"/></p>
+            <p>Candy</p>
+            <p>Description</p>
+            <p>Price</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="seating">
+        <h1>
+          State of the Art Seating
+          <img src={seats} alt="seats" width="75%"/>
+          </h1>
+      </div>
+    </body>
   );
 }
 
