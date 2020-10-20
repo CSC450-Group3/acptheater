47d242af5d470bf5bfca166c97846855ad40affd
Alexyahn <48939637+Alexyahn@users.noreply.github.com>
2020-10-19 19:56:03 -0500

    Very beginning of the Scheduling form

---
+++ src/frontend/pages/ScheduleForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function ScheduleForm() {
+  return (
+    <form>
+    <h1>Schedule Form</h1>
+    <p>Screen</p>
+    <input
+      type="text"
+    />
+  </form>
+  );
+}
+
+export default ScheduleForm;
