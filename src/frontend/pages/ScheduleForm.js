4343adeda5deab8ed18b6c04205384420e62ee6f
Alexyahn <48939637+Alexyahn@users.noreply.github.com>
2020-10-20 20:10:22 -0500

    Changing functionality of the "Schedule Movies"
    Just changed the functionality of the Schedule Movies button.

--- src/frontend/pages/ScheduleForm.js
+++ src/frontend/pages/ScheduleForm.js
@@ -1,15 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Modal, Button } from 'antd';
+import { Link } from "react-router-dom";
 
 
 function ScheduleForm() {
+
+const [activateModal, setActivateModal] = useState(true);
+const [backToMovies, setBackToMovies] = useState(false);
+
   return (
-    <form>
-    <h1>Schedule Form</h1>
-    <p>Screen</p>
-    <input
-      type="text"
-    />
-  </form>
+	<div className="ScheduleForm">
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		
+		<Modal
+			title='Schedule Form'
+			centered
+			visible={activateModal}
+			onCancel={() => setActivateModal(false)}
+			onOk={() => setBackToMovies(true)}
+			width={215}
+			footer={[
+				<Button key="cancel" onClick={() => setActivateModal(false)}><Link to='/Movies'>Cancel</Link >
+				</Button>,
+				<Button key="schedule" onClick={() =>setBackToMovies(true)}><Link to='/Movies'>Schedule</Link ></Button>
+				]}
+			>
+			<form>
+				
+				<p>Screen</p>
+				<input
+				type="text"
+				/>
+				<p>Start Date and Time</p>
+				<input
+				type="tezt"
+				/>
+				<p>Price</p>
+				<input
+				type="tezt"
+				/>
+			</form>
+			</Modal>
+
+	</div>
   );
 }
 
