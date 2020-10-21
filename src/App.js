d09a64fc146087c80738e7205d424077f34e11ef
PGarryCS <65678578+PGarryCS@users.noreply.github.com>
2020-10-21 18:02:55 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './frontend/pages/Home';
 import Movies from './frontend/pages/Movies';
+import SignUp from './frontend/pages/SignUp';
+import Login from './frontend/pages/Login';
 import NavbarUser from './frontend/components/app/NavbarUser';
 import NavbarAdmin from './frontend/components/app/NavbarAdmin';
 import NavbarHome from './frontend/components/app/NavbarHome';
@@ -11,10 +13,15 @@ import './App.css';
 function App() {
   return (
     <Router>
-        <NavbarAdmin />
+        <NavbarHome />
+        {/*<NavbarUser />
+        <NavbarAdmin />*/}
       <div className="App">
           <Route exact path="/"><Home /> </Route>
+          <Route exact path="/Home"><Home /> </Route>
           <Route exact path="/Movies"><Movies /> </Route>
+          <Route exact path="/SignUp"><SignUp /> </Route>
+          <Route exact path="/Login"><Login /> </Route>
       </div>
         <Footer/>
     </Router>
