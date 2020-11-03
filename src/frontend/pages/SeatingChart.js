2c921d2253e5c9a8749a2f0df49b842d385aa3c8
Alexyahn <48939637+Alexyahn@users.noreply.github.com>
2020-11-03 14:51:52 -0600

    Adding to Seating Chart
    Added the actual seating chart where each button is a seat and it is clickable. Still need to find the flow of pages from Showtimes -> checking out. What do we want the flow to be? Right now it's Showtimes > Purchase Tickets > Seating chart. This can be changed however we need it to be.

--- src/frontend/pages/SeatingChart.js
+++ src/frontend/pages/SeatingChart.js
@@ -19,14 +19,63 @@ const [nextPage, setNextPage] = useState(false);
 			width={400}
 			footer={[
 				<Button key="cancel" onClick={() => setActivateModal(false)}><Link to='/Showtimes'>Cancel</Link > </Button>,
-				<Button key="chooseSeat" onClick={() => setNextPage(true)}><Link to='/NextPage'>Confirm Seat</Link > </Button>
+				<Button key="chooseSeat" onClick={() => setNextPage(true)}><Link to='/NextPage'>Confirm Seats</Link > </Button>
 				]}
 			>
-			<form>
-                <p>This will be the form to Select Seats</p>
-			</form>
+				<form>
+					<h1>___________Screen___________</h1>
+					<Button style={{padding: 7}}>A1</Button>
+					<Button style={{padding: 7}}>A2</Button>
+					<Button style={{padding: 7}}>A3</Button>
+					<Button style={{padding: 7}}>A4</Button>
+					<Button style={{padding: 7}}>A5</Button>
+					<Button style={{padding: 7}}>A6</Button>
+					<Button style={{padding: 7}}>A7</Button>
+					<Button style={{padding: 7}}>A8</Button>
+					<Button style={{padding: 7}}>A9</Button>
+					<Button style={{padding: 7}}>A10</Button>
+					<Button style={{padding: 7.5}}>B1</Button>
+					<Button style={{padding: 7.5}}>B2</Button>
+					<Button style={{padding: 7.5}}>B3</Button>
+					<Button style={{padding: 7.5}}>B4</Button>
+					<Button style={{padding: 7.5}}>B5</Button>
+					<Button style={{padding: 7.5}}>B6</Button>
+					<Button style={{padding: 7.5}}>B7</Button>
+					<Button style={{padding: 7.5}}>B8</Button>
+					<Button style={{padding: 7.5}}>B9</Button>
+					<Button style={{padding: 7.5}}>B10</Button>
+					<Button style={{padding: 7.2}}>C1</Button>
+					<Button style={{padding: 7.2}}>C2</Button>
+					<Button style={{padding: 7.2}}>C3</Button>
+					<Button style={{padding: 7.2}}>C4</Button>
+					<Button style={{padding: 7.2}}>C5</Button>
+					<Button style={{padding: 7.2}}>C6</Button>
+					<Button style={{padding: 7.2}}>C7</Button>
+					<Button style={{padding: 7.2}}>C8</Button>
+					<Button style={{padding: 7.2}}>C9</Button>
+					<Button style={{padding: 7.2}}>C10</Button>
+					<Button style={{padding: 6.7}}>D1</Button>
+					<Button style={{padding: 6.7}}>D2</Button>
+					<Button style={{padding: 6.7}}>D3</Button>
+					<Button style={{padding: 6.7}}>D4</Button>
+					<Button style={{padding: 6.7}}>D5</Button>
+					<Button style={{padding: 6.7}}>D6</Button>
+					<Button style={{padding: 6.7}}>D7</Button>
+					<Button style={{padding: 6.7}}>D8</Button>
+					<Button style={{padding: 6.7}}>D9</Button>
+					<Button style={{padding: 6.7}}>D10</Button>
+					<Button style={{padding: 8}}>E1</Button>
+					<Button style={{padding: 8}}>E2</Button>
+					<Button style={{padding: 8}}>E3</Button>
+					<Button style={{padding: 8}}>E4</Button>
+					<Button style={{padding: 8}}>E5</Button>
+					<Button style={{padding: 8}}>E6</Button>
+					<Button style={{padding: 8}}>E7</Button>
+					<Button style={{padding: 8}}>E8</Button>
+					<Button style={{padding: 8}}>E9</Button>
+					<Button style={{padding: 8}}>E10</Button>
+				</form>
 			</Modal>
-
 	</div>
   );
 }
